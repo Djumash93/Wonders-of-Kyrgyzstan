6206e16c03c4d5d124e8061ce35a5535989c1ea4
Djumash93 <djumash.violin@gmail.com>
2023-11-18 18:23:29 +0000

    updates api

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 const app = express();
-
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res
 //     .status(200) //when no status, 200 is the default
@@ -13,7 +17,32 @@ const app = express();
 // });
 
 app.get('/api/v1/tours', (req, res) => {
-  res.send();
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 const port = 3000;
 app.listen(port, () => {
